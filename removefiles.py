70cdbded460fb1bb63a283b83bd4a10a1a83c364
unknown <lee078bg@yahoo.com>
2020-04-28 15:20:29 -0700

    hii

--- removefiles.py
+++ removefiles.py
@@ -6,9 +6,10 @@ for root, dirs, files in os.walk(".", topdown=False):
     # for name in files:
     #    print(os.path.join(root, name))
     for name in dirs:
-        if name == "dataset":
-            print("Dataset dir exists")
-    # print(os.path.join(root, name))
+        # if name == "dataset":
+        #     print("Dataset dir exists")
+        print(os.path.join(root, name))
+        print(name)
 
 # directory_name="sampleenv"
 
