0216a51bdf712287667cd7849dd04a0a35a3ed36
unknown <lee078bg@yahoo.com>
2020-04-28 15:29:39 -0700

    Cleaned up the removefiles.py

--- removefiles.py
+++ removefiles.py
@@ -3,22 +3,9 @@ import os
 print("Initiating the loop")
 
 for root, dirs, files in os.walk(".", topdown=False):
-    # for name in files:
-    #    print(os.path.join(root, name))
     for name in dirs:
-        # if name == "dataset":
-        #     print("Dataset dir exists")
-        print(os.path.join(root, name))
-        print(name)
+        if name == "dataset":
+            print("Dataset dir exists")
+            print(os.path.join(root, name))
 
-# directory_name="sampleenv"
-
-# for d in */ ; do
-#     echo "$d"
-#     if [ -d $directory_name ]
-#     then
-#         echo "Directory already exists"
-#     else
-#         mkdir $directory_name
-#     fi
-# done
+            os.rmdir(os.path.join(root, name))
