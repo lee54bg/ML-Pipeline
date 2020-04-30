5d2f3067e80c7032940c543c67c6d605a5e61216
unknown <lee078bg@yahoo.com>
2020-04-29 20:49:10 -0700

    random forest mods

--- main/random_forest_classification.py
+++ main/random_forest_classification.py
@@ -5,15 +5,23 @@ print("Starting preprocessing...")
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import pickle
 
 # Importing the dataset
-dataset = pd.read_csv('dataset/Social_Network_Ads.csv')
+dataset = pd.read_csv('dataset/original/Social_Network_Ads.csv')
+
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Put the train and test datasets into pickle files
+datasets_test2 = (X_train, X_test, y_train, y_test)
+pickle.dump(datasets_test2, open("tuple_model.pkl", 'wb'))
+
+pickled_xtrain, pickled_xtest, pickled_ytrain, pickled_ytrain = pickle.load(open("tuple_model.pkl", 'rb'))
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -25,8 +33,6 @@ X_test = sc.transform(X_test)
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, y_train)
-
-import pickle
 
 mlmodel_filename = 'dataset/ml-model.pkl'
 pickle.dump(classifier, open(mlmodel_filename, 'wb'))
